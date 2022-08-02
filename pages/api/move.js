e0be8e113acc9369f033970712d7bfcacfe5680f
Mukul <mukulz2020@gmail.com>
2022-08-02 23:14:11 +0530

    first commit

---
+++ pages/api/move.js
@@ -0,0 +1,31 @@
+import mqtt from 'mqtt';
+
+const client = mqtt.connect('mqtt://broker.hivemq.com');
+
+export default function handler(req, res) {
+	try {
+		if (req.method !== 'POST') throw 'Only POST requests allowed';
+
+		const dir = req.body.dir;
+		const on = req.body.on;
+
+		if (!['f', 'r', 'b', 'l'].includes(dir) || (on !== true && on !== false))
+			throw `Invalid Request Body: ${JSON.stringify(req.body)}`;
+
+		const codes = {
+			f: 'Forward',
+			r: 'Right',
+			b: 'Backward',
+			l: 'Left'
+		};
+
+		client.publish('internet', `${dir}${on}`, console.log);
+
+		console.log(`${codes[dir]} ${on ? 'ON' : 'OFF'}`);
+
+		res.send('Success!');
+	} catch (error) {
+		console.log(error);
+		res.send('Error!');
+	}
+}
